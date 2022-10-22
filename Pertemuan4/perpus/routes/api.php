10b1cbf112c62ef555aba4ac764470a830dfc6c5
Ibnu030303 <114752641+Ibnu030303@users.noreply.github.com>
2022-10-22 12:24:37 +0700

    Add files via upload

--- Pertemuan4/perpus/routes/api.php
+++ Pertemuan4/perpus/routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\BiodataController;
+use App\Http\Controllers\PustakawanController;
+use GuzzleHttp\Psr7\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Nette\Utils\Json;
+use SebastianBergmann\CodeCoverage\Node\CrapIndex;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// CARA 1
+// Route::get('/pustakawan', function() {
+//     return response()->json([
+//         "message" => "hello world"
+//      ], 200);
+// });
+
+// Cara lebih rapih
+// Route untuk menampilkan data pustakawan
+Route::get('/pustakawan', [PustakawanController::class, 'index']); 
+
+// Tugas Pertemuan6 membuat route
+Route::get('/biodata', [BiodataController::class, 'index']); 
+
+// Get all resource
+Route::get('/books', [Bookscontroller::class, 'index']);
